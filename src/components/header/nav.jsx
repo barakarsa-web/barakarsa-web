7e13912eda464403408ac8ec8da3947fef593208
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2024-05-13 15:43:41 +0700

    finish for navbar

--- src/components/header/nav.jsx
+++ src/components/header/nav.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from 'react'
 import { Menu, X } from 'lucide-react'
-import Navlinks from './navlinks'
+import { NavLink } from 'react-router-dom'
+
+
+const Navlinks = () => {
+    return (
+        <>
+            <NavLink to='/' className='font-bold text-secondary-300'>Home</NavLink>
+            <NavLink to='/about' className='font-bold text-secondary-300'>Tentang Kami</NavLink>
+            <NavLink to='/contact' className='font-bold text-secondary-300'>Contact</NavLink>
+            <NavLink to='/blog' className='font-bold text-secondary-300'>Blog</NavLink>
+        </>
+    )
+}
 
 const Nav = () => {
     const [IsOpen, setIsOpen] = useState(false)
@@ -11,22 +23,20 @@ const Nav = () => {
 
     return (
         <>
-            <div className='w-1/3 flex justify-end'>
-                <div></div>
-                <div className='hidden w-full justify-between md:flex'>
+            <nav className='w-1/3 transition duration-150 ease-in-out  flex-wrap flex justify-end'>
+                <div className='hidden w-full  justify-between md:flex'>
                     <Navlinks />
                 </div>
                 <div className='md:hidden'>
                     <button onClick={toggleNavbar} className='focus:outline-none'>
-                        {IsOpen ? <X size='24' /> : <Menu size='24' />}
+                        {IsOpen ? <X size='32' color='#A3722A' /> : <Menu size='32' color='#A3722A' />}
                     </button>
                 </div>
-                {IsOpen && (
-                    <div className='flex  basis-full flex-col items-center'>
-                        <Navlinks />
-                    </div>
-                )}
-            </div>
+                <div className={`transition-all duration-300 ease-in-out flex md:hidden basis-full flex-col items-center gap-9 justify-end 
+                    ${IsOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'}`}>
+                    <Navlinks />
+                </div>
+            </nav>
         </>
 
     )
