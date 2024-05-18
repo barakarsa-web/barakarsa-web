9ab21ac77b83679888a9f3d3a0591867240ff6d2
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2024-05-18 16:15:53 +0700

    add some animation in navbar and make it fixed position

--- src/components/header/hero.jsx
+++ src/components/header/hero.jsx
@@ -11,9 +11,9 @@ const Hero = () => {
             </video>
             {/* Overlay untuk membuat video gelap */}
             <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-70"></div>
-            <div className="absolute top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 text-center z-10">
-                <img src={text} className="md:w-[60%] m-auto md:h-full"></img>
-                <p className="text-lg md:mt-2 text-secondary-100 font-medium font-['Young_serif'] md:text-xl">Bakti dan berkarya untuk bangsa</p>
+            <div className="absolute top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 text-center z-10 ">
+                <img src={text} className="md:w-[60%] m-auto md:h-full  animate-backInDown"></img>
+                <p className="text-lg md:mt-2 text-secondary-100 font-medium font-['Young_serif'] animate-backInTop md:text-xl">Bakti dan berkarya untuk bangsa</p>
             </div>
         </div >
     );
