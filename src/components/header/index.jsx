3bdb3fdd757f50538cbb5dd75e86c3bb663422bb
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2024-05-19 15:43:38 +0700

    add image and text in section about

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <header className='relative'>
             <Hero />
-            <div className={`fixed  top-0 left-0 right-0 flex justify-between md:p-10 md:px-20 py-10 px-14 mx-auto `}>
+            <div className={`fixed   top-0 left-0 right-0 flex justify-between md:p-10 md:px-20 py-10 px-14 mx-auto `}>
                 <Logo />
                 <Nav />
             </div>
